6a7e325db0244728e992ba162416b9457f335a0c
Urpagin <passieuxmattis@gmail.com>
2023-07-18 05:11:27 +0200

    updated readme, fixed many bugs, added features

--- README.md
+++ README.md
@@ -1,7 +1,73 @@
 # SelfbotDiscordClear
-A small Discord selfbot that allows the deletion of own messages.
 
+A neat Discord selfbot that allows the deletion of own messages.
 
+## Table of contents
 
+- [Installation](#Installation)
+- [Usage](#usage)
+- [Common Errors](#common-errors)
+- [Legal](#Legal)
 
-![cactus](https://github.com/Urpagin/SelfbotDiscordClear/assets/72459611/9def68f1-8eac-434b-aee9-2863eb65bafd)
+## Installation
+
+### 🪟 Windows
+
+1. Download the latest release [here](https://github.com/Urpagin/SelfbotDiscordClear/releases/latest)
+2. Get your Discord token
+    <details>
+      <summary>How to get your Discord token</summary>
+   <p><a href="https://www.youtube.com/watch?v=LnBnm_tZlyU">https://www.youtube.com/watch?v=LnBnm_tZlyU</a><br>
+   <a href="https://www.youtube.com/watch?v=LnBnm_tZlyU">(mirror) https://www.youtube.com/watch?v=LnBnm_tZlyU</a></p>
+    </details>
+3. Paste your Discord token into the app's terminal
+4. See [usage](#Usage)
+
+### 🐧 Linux/Mac/Source 🍎
+
+You'll need to build from source
+
+1. Install [Python](https://www.python.org/downloads/)
+2. Clone the repository <br>
+   ```shell
+   git clone https://github.com/Urpagin/SelfbotDiscordClear.git
+   ```
+3. Go in the directory SelfbotDiscordClear and install dependencies
+   ```shell
+   pip install -r requirements.txt
+   ```
+4. Launch the app
+   ```shell
+   python main.py
+   ```
+5. Get your token in the Windows section then see [Usage](#Usage)
+
+## Usage
+
+Once the app is successfully running:
+
+### ⚒️ How to use ⚒️
+
+- `!clearr <amount> <links>`
+    - `<amount>`: (integer) the amount of own messages to delete
+    - `<links>`: (boolean) if true, deletes only messages containing links
+
+### 📜 Examples 📜
+
+- `!clearr 10 1` will delete 10 of your messages containing links
+- `!clearr -1` will delete ALL of your messages (may take a long time)
+- `!clearr -1 1` will delete ALL of you messages that contain links (may take a long time)
+
+## Common errors
+
+1. In the terminal you might see <br>
+   `WARNING  discord.gateway Gateway is ratelimited, waiting 57.50 seconds.`<br>
+   You may not be able to delete any messages during the timespan specified, wait and it will pass.
+2. In the terminal you might se <br>
+   `WARNING  discord.http We are being rate limited. DELETE https://discord.com/api/v9/channels/<id>/messages/<id> responded with 429. Retrying in 1.53 seconds.`<br>
+   This is a non issue as mass deleting messages isn't the normal behavior of a user, wait and it will pass.
+
+## Legal
+Selfbots are against Discord's Terms of Service.<br>
+> **Automating normal user accounts (generally called "self-bots") outside of the OAuth2/bot API is forbidden, and can result in an account termination if found.**<br>
+> https://support.discord.com/hc/en-us/articles/115002192352-Automated-user-accounts-self-bots-
--- main.py
+++ main.py
@@ -1,43 +1,67 @@
+from typing import AsyncGenerator
+
 import discord.ext.commands
 from discord.ext import commands
 
 bot: discord.ext.commands.bot.Bot = commands.Bot(command_prefix='!', self_bot=True)
 
 
+async def get_message(ctx: discord.ext.commands.Context,
+                      amount: int, links: bool) -> AsyncGenerator[discord.Message, None]:
+    """Gets the messages in a channel. If `amount` < 0 gets all messages.
+     If `links` returns only messages containing links"""
+    count = 0
+    async for message in ctx.channel.history(limit=None):
+        if count == amount:
+            return
+        if message.author != ctx.author:
+            continue
+        if links:
+            if 'http://' in message.content or 'https://' in message.content:
+                count += 1
+                yield message
+            continue
+        else:
+            count += 1
+            yield message
+
+
+async def delete_messages(messages: list[discord.Message, ...]):
+    count = 0
+    for message in messages:
+        await message.delete()
+        count += 1
+
+
 @bot.command()
 async def clearr(ctx: discord.ext.commands.Context, amount: int = 0, links: bool = False):
     if ctx.me.id != bot.user.id:  # Only owner can interact
         return
+    await ctx.message.delete()
     if not amount:
-        await ctx.send('Please specify integer argument `amount` of messages to delete. `!clearr <amount> <links>`')
+        await ctx.send('# :tools: How to use :tools:\n- `!clearr <amount> <links>`\n'
+                       '  - `<amount>`: (integer) the amount of own messages to delete\n'
+                       '  - `<links>`: (boolean) if true, deletes only messages containing links\n'
+                       '# :scroll: Examples :scroll:\n'
+                       '- `!clearr 10 1` will delete 10 of your messages containing links\n'
+                       '- `!clearr -1` will delete ALL of your messages (may take a long time)', silent=True)
         return
 
-    messages = []
-    async for message in ctx.channel.history(limit=None):
-        if len(messages) == amount + 1:  # +1 to delete sent command msg
-            break
-        if message.author == ctx.author:
-            if links:
-                if 'http://' in message.content or 'https://' in message.content:
-                    messages.append(message)
-                continue
-            messages.append(message)
+    count = 0
+    async for message in get_message(ctx, amount, links):
+        await message.delete()
+        count += 1
 
-    try:
-        await ctx.channel.delete_messages(messages)
-        return
-    except Exception as e:
-        print(e)
-        for msg in messages:
-            await msg.delete()
+        print(f'Deleted {count}/{"all" if amount < 0 else amount} messages.'
+              f' (note that `{"all" if amount < 0 else amount}` is theoretical)')
 
-    await ctx.send(f'Deleted {len(messages) - 1} of your messages.', delete_after=5)
+    print(f'---- Task done, deleted {count} {"message" if count == 1 else "messages"} ----')
+    await ctx.send(f':saluting_face: Deleted `{count}` of your messages.', delete_after=5, silent=True)
 
 
 if __name__ == '__main__':
     while True:
-        token: str = input('Enter your Discord token: ')
-        token = token.replace('"', '').strip()
+        token: str = input('Enter your Discord token: ').replace('"', '').strip()
         if token:
             break
 
